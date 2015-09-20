d18790016f125d9bdabe22683b8b86c1f615c4a3
Sara Bee <sara.bainbridge@gmail.com>
2015-09-19 21:12:45 -0400

    Add external Toke module

--- test/toke-test.js
+++ test/toke-test.js
@@ -1,8 +1,9 @@
 var expect = require("chai").expect;
-
+var Toke = require("../../toke");
 
 describe("toke", () => {
   it("does anything", () => {
+    var t = new Toke({ lex: "hi" });
     expect(true).to.eql(true);
   });
 
---
+++ toke.js
@@ -0,0 +1,5 @@
+export default class {
+  constructor(opts) {
+    console.log(opts);
+  }
+}
